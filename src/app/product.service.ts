537f0e5cb2c0e24f9b8370f878919f46426edf2e
bka4 <74060676+bka4@users.noreply.github.com>
2020-11-06 11:25:58 -0600

    Add files via upload

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,13 +4,12 @@ import rxjs/add/operator/map;
 
 @Injectable()
 export class ProductService {
-  albumInfo;
 
   constructor(private _http: Http) { }
-  private _albumUrl = "../assets/album.json";
+  private _albumUrl = '../assets/album.json';
 
-  method getAlbum(id:number) {
-    return this._http.get(_albumUrl);
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map((response) => response.json());
   }
 
 }
