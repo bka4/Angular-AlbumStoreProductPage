1751397d9fc47ae416f9dd874ec044f508297f83
bka4 <74060676+bka4@users.noreply.github.com>
2020-11-06 11:50:21 -0600

    Add files via upload

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
-import 'rxjs/Observable';
+import {Observable} from 'rxjs/Observable';
 import {Album} from './album';
 
 @Injectable()
@@ -10,7 +10,7 @@ export class ProductService {
   constructor(private _http: Http) { }
   private _albumUrl = '../assets/album.json';
 
-  getAlbum(id: number): Observable<Album>) {
+  getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
 
